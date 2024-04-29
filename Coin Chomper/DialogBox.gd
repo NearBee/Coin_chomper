bf673de037e7df4123c27750653d89afb3b903e7
Nathaniel Berube <100102632+NearBee@users.noreply.github.com>
2024-04-29 06:34:15 -0500

    Added Double Jump + Wall Jump, Experimental movement

--- Coin Chomper/DialogBox.gd
+++ Coin Chomper/DialogBox.gd
@@ -1,31 +1,31 @@
 extends Control
-
-@onready var timer = $VisibilityTimer
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	# Start the animation when the dialog box appears
-	animate_in()
-	animate_out()
-	
-
-func animate_in():
-	var tween = get_tree().create_tween()
-	tween.interpolate_value(self, 0.0, 1.0, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	tween.start()
-	
-	# Start the tiemr to close the dialog box after 2 seconds
-	$Timer.start()
-
-func animate_out():
-	var tween = get_tree().create_tween()
-	tween.interpolate_value(self, 1.0, 0.0, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	tween.start()
-	
-	# Disconnect signals and clean up resources
-	$Timer.stop()
-	$Timer.disconnect('timeout', timer)
-	
-
-func _on_Timer_timeout():
-	animate_out()
+#
+#@onready var timer = $VisibilityTimer
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+	## Start the animation when the dialog box appears
+	#animate_in()
+	#animate_out()
+	#
+#
+#func animate_in():
+	#var tween = get_tree().create_tween()
+	#tween.interpolate_value(self, 0.0, 1.0, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	#tween.start()
+	#
+	## Start the tiemr to close the dialog box after 2 seconds
+	#$Timer.start()
+#
+#func animate_out():
+	#var tween = get_tree().create_tween()
+	#tween.interpolate_value(self, 1.0, 0.0, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	#tween.start()
+	#
+	## Disconnect signals and clean up resources
+	#$Timer.stop()
+	#$Timer.disconnect('timeout', timer)
+	#
+#
+#func _on_Timer_timeout():
+	#animate_out()
--- Coin Chomper/player.gd
+++ Coin Chomper/player.gd
@@ -7,6 +7,8 @@ extends CharacterBody2D
 #@export var dialog_box : DialogBox  TO BE WORKED ON SOON
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
+var air_jump = false
+var just_wall_jumped = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var coyote_jump_timer = $"Coyote Jump Timer"
@@ -15,8 +17,10 @@ func _physics_process(delta):
 	var input_axis = Input.get_axis("ui_left", "ui_right")
 	
 	apply_gravity(delta)
+	handle_wall_jump()
 	handle_jump()
 	handle_acceleration(input_axis, delta)
+	handle_air_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	apply_air_resistance(input_axis, delta)
 	update_animations(input_axis)
@@ -25,10 +29,13 @@ func _physics_process(delta):
 	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
 	if just_left_ledge:
 		coyote_jump_timer.start()
+	just_wall_jumped = false
+		
 	if Input.is_action_just_pressed("ui_accept") and movement_data.resource_name != "res://FasterMovementData.tres":		
 		movement_data = load("res://FasterMovementData.tres")
 		visibility_timer.start()
 		#dialog_box.label.set_visible(true)  TO BE WORKED ON SOON
+	
 		
 
 
@@ -36,8 +43,18 @@ func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+func handle_wall_jump():
+	if not is_on_wall_only(): return
+		
+	var wall_normal = get_wall_normal()
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.x = wall_normal.x * movement_data.speed
+		velocity.y = movement_data.jump_velocity
+		just_wall_jumped = true
 
 func handle_jump():
+	if is_on_floor(): air_jump = true
+	
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = movement_data.jump_velocity
@@ -45,6 +62,9 @@ func handle_jump():
 		if Input.is_action_just_released("ui_up") and velocity.y < movement_data.jump_velocity / 2:
 			velocity.y = movement_data.jump_velocity / 2
 			
+		if Input.is_action_just_pressed("ui_up") and air_jump and not just_wall_jumped:
+			velocity.y = movement_data.jump_velocity
+			air_jump = false
 
 func apply_friction(input_axis, delta):
 	if input_axis == 0 and is_on_floor():
@@ -55,8 +75,14 @@ func apply_air_resistance(input_axis, delta):
 		velocity.x = move_toward(velocity.x, 0, movement_data.air_resistance * delta)
 
 func handle_acceleration(input_axis, delta):
+	if not is_on_floor(): return
 	if input_axis != 0:
 		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.acceleration * delta)
+
+func handle_air_acceleration(input_axis, delta):
+	if is_on_floor(): return
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, movement_data.speed * input_axis, movement_data.air_acceleration * delta)
 
 func update_animations(input_axis):
 	if input_axis != 0:
